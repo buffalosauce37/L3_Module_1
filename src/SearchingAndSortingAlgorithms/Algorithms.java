537a0ccb4f72cdee0cd1a65adc010d01a753d0fc
ThirdTime <league@WorthyMini.local>
2018-04-25 18:09:51 -0700

    I worked on my Sorting Algorithms thing.

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -21,5 +21,38 @@ public class Algorithms {
 		}
 		return tRex;
 	}
+
 	// Add other methods here
+	public static Double findTallest(List<Double> peeps) {
+		Double UtahRaptor = (double) 0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) >= UtahRaptor) {
+				UtahRaptor = peeps.get(i);
+			}
+		}
+		return UtahRaptor;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String longestWord = null;
+		int longestLength = 0;
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longestLength) {
+				longestLength = words.get(i).length();
+				longestWord = words.get(i);
+			}
+		}
+		return longestWord;
+	}
+	public static Boolean containsSOS(List<Boolean> words) {
+		Boolean messagesss;
+		int longestLength = 0;
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longestLength) {
+				longestLength = words.get(i).length();
+				longestWord = words.get(i);
+			}
+		}
+		return longestWord;
+	}
 }
